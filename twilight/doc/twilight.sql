81dd0d6b9a4ce8c8c08c64d65a4af146c4045349
wang <wangfeiping@outlook.com>
2019-03-01 14:57:55 +0800

    change sql

--- twilight/doc/twilight.sql
+++ twilight/doc/twilight.sql
@@ -11,9 +11,24 @@
  * -- 功能：
  *     充值、提现
  *
+ * funds (api funds)
+ * -- 双币基金，一部分作为运营费用抵押，一部分作为承兑基金
+ * -- 功能：
+ *     生成联盟链记录地址 - funds_address；
+ *     配置比例，调用联盟链合约计入联盟链；
+ *     仅用于记录总账，资金详细记录另计！！！
+ *
  * quotation (api quotation)
  * -- token 牌价
  *     牌价查询；token 兑换
+ *
+ * chips (api chips)
+ * -- 筹码
+ * -- 功能
+ *     在游戏开始计算最大公约数，用于筹码兑换；
+ *     记录游戏中的兑换比率，调用联盟链合约，记录上链
+ *     游戏开始时自动兑换，
+ *     游戏结束，或用户离场时，自动回兑；
  *
  * game (api game)
  * -- 游戏，用于记录游戏合约（联盟链合约），用户用来查看游戏项目和选择游戏
@@ -87,11 +102,35 @@ CREATE TABLE `wallet_address` (
 ) ENGINE=InnoDB AUTO_INCREMENT=264 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
+-- Table structure for funds
+-- ----------------------------
+DROP TABLE IF EXISTS `funds`;
+CREATE TABLE `funds` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'funds id',
+  `user_id` int(11) NOT NULL,
+  `base_network` varchar(32) DEFAULT NULL,
+  `base_token_name` varchar(32) DEFAULT NULL,
+  `base_balance` int(64) DEFAULT '0',
+  `mortgage_balance` int(64) DEFAULT '0',
+  `exchange_balance` int(64) DEFAULT '0',
+  `token_network` varchar(32) DEFAULT NULL,
+  `token_name` varchar(32) DEFAULT NULL,
+  `token_balance` int(64) DEFAULT '0',
+  `funds_address` varchar(255) NOT NULL,
+  `status` int(4) DEFAULT '0',
+  `createtime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `remarks` varchar(64) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unique_funds` (`base_network`,`base_token_name`,`token_network`,`token_name`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=264 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
 -- Table structure for quotation
 -- ----------------------------
 DROP TABLE IF EXISTS `quotation`;
 CREATE TABLE `quotation` (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'token quotation id',
+  `base_network` varchar(32) DEFAULT NULL,
   `base_token_name` varchar(32) DEFAULT NULL,
   `network` varchar(32) DEFAULT NULL,
   `token_name` varchar(32) DEFAULT NULL,
@@ -101,6 +140,25 @@ CREATE TABLE `quotation` (
   `remarks` varchar(64) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `token_base` (`token_name`,`network`,`base_token_name`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=264 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for chips
+-- ----------------------------
+DROP TABLE IF EXISTS `chips`;
+CREATE TABLE `chips` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'chips id',
+  `gaming_seq` varchar(32) DEFAULT NULL COMMENT 'sequence of game　in progress',
+  `wallet_address` varchar(255) NOT NULL,
+  `network` varchar(32) DEFAULT NULL,
+  `token_name` varchar(32) DEFAULT NULL,
+  `token_balance` int(64) DEFAULT '0',
+  `chips_balance` int(64) DEFAULT '0',
+  `status` int(4) DEFAULT '0',
+  `createtime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `remarks` varchar(64) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `gaming_chips` (`gaming_seq`,`wallet_address`) USING BTREE
 ) ENGINE=InnoDB AUTO_INCREMENT=264 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
